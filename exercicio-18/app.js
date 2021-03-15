20ec479cc4080b59f4b7f3c2fde2d43006a17f0b
Gabriel Merigo <gabriel.merigo.dev@gmail.com>
2021-03-15 20:57:51 -0300

    Update app.js

--- exercicio-18/app.js
+++ exercicio-18/app.js
@@ -22,27 +22,39 @@
 */
 const form = document.querySelector('form');
 const regex = /^[a-zA-Z]{6,}/
-const input = form.username; //border-success
+const input = form.username;
+
 
 input.addEventListener('keyup', () => {
   const inputValue = input.value;
-  if(regex.test(inputValue)){
-    return input.setAttribute('class','border-success')
+
+  if (regex.test(inputValue)) {
+    return input.setAttribute('class', 'border-success')
   }
-  return input.setAttribute('class','border-error')
+  return input.setAttribute('class', 'border-error')
 })
 
-form.addEventListener('submit', (() => {
+
+form.addEventListener('submit', event => {
+  event.preventDefault()
+  const inputValue = input.value;
   
+  if (!regex.test(inputValue)) {
+   return input.insertAdjacentHTML('afterend', `<p class="username-help-feedback">O valor deve conter no mínimo 6 caracteres,  
+   com apenas letras maiúsculas e/ou minúsculas</p>`)
+  }
+
+  return input.insertAdjacentHTML('afterend', `<p class="username-success-feedback">Username válido =)</p>`)
+
 })
 
 /*
   02
 
   - Valide o envio do form;
-  - Se o username inserido no input é válido, no envio do form, exiba um  
+  - Se o username inserido no input é válido, no envio do form, exiba um
     parágrafo verde abaixo do botão com a mensagem "Dados enviados =)";
-  - Se no momento do envio, o valor do input é inválido, o parágrafo deve ser  
+  - Se no momento do envio, o valor do input é inválido, o parágrafo deve ser
     vermelho e exibir "Por favor, insira um username válido".
   - Use as classes disponíveis no arquivo style.css para colorir o parágrafo;
   - Não insira o parágrafo manualmente no index.html.
@@ -53,7 +65,7 @@ form.addEventListener('submit', (() => {
 
   - Há algumas aulas, falamos sobre o método some;
   - Neste exercício, seu desafio será criar este método do zero;
-  - Implemente uma função "some" que possui a mesma funcionalidade do método  
+  - Implemente uma função "some" que possui a mesma funcionalidade do método
     some original;
   - A assinatura da invocação desta função deverá ser:
     - some([1, 2, 3], item => item > 2) - Retorna true;
@@ -61,10 +73,10 @@ form.addEventListener('submit', (() => {
   - Se você não se lembra como o método some funciona, há 2 opções:
     1) Reassistir às seguintes aulas:
       - "Desenvolvendo um popup" - Aula 04-04 da etapa 5;
-      - "Correção dos exercícios da aula 04 da etapa 05" - Aula 01-01 da etapa  
+      - "Correção dos exercícios da aula 04 da etapa 05" - Aula 01-01 da etapa
         6;
     2) Pesquisar no MDN.
-  
-  Spoiler alert: este tipo de exercício será frequente em etapas mais avançadas  
+
+  Spoiler alert: este tipo de exercício será frequente em etapas mais avançadas
   do curso, onde falaremos sobre TDD. Vá se aquecendo =D
 */
